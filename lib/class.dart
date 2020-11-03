20f3bad326fb5928eeaaa7639d679409b7a83029
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2020-11-03 16:23:21 -0600

    refactor(init): Remove scaffold from func widget

--- lib/class.dart
+++ lib/class.dart
@@ -21,40 +21,34 @@ class _LDState extends State<LD> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: GestureDetector(
-            onTap: () {
-              findRandom.checkLie();
-              setState(() {
-                bck = findRandom.getRandomColor();
+    return GestureDetector(
+      onTap: () {
+        findRandom.checkLie();
+        setState(() {
+          bck = findRandom.getRandomColor();
 
-                if (findRandom.checkLie() == true) {
-                  displayText = methods.foundLie();
-                  return sound.play("lie");
-                } else {
-                  displayText = methods.foundTrue();
-                  return sound.play("truth");
-                }
-              });
-            },
-            child: Container(
-              margin: const EdgeInsets.all(10.0),
-              width: 250.0,
-              height: 250.0,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(120),
-                color: bck,
-              ),
-              child: Center(
-                child: Text(
-                  displayText,
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.w800),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ),
+          if (findRandom.checkLie() == true) {
+            displayText = methods.foundLie();
+            return sound.play("lie");
+          } else {
+            displayText = methods.foundTrue();
+            return sound.play("truth");
+          }
+        });
+      },
+      child: Container(
+        margin: const EdgeInsets.all(10.0),
+        width: 250.0,
+        height: 250.0,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(120),
+          color: bck,
+        ),
+        child: Center(
+          child: Text(
+            displayText,
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.w800),
+            textAlign: TextAlign.center,
           ),
         ),
       ),
